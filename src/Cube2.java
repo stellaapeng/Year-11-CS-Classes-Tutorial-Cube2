167d6c8d33752524a80d029a093a3867bbe41dd8
Stella <stellap2028@student.cis.edu.hk>
2025-12-05 11:32:41 +0800

    fujlblih

--- src/Cube2.java
+++ src/Cube2.java
@@ -12,18 +12,73 @@ public class Cube2 {
         basicCube.setSide(side);
     }
 
+    public Cube2(int side, String color) {
+        this();
+        basicCube.setSide(side);
+        this.setColor(color);
+    }
+
     public int getSide() {
         return basicCube.getSide();
     }
 
-    //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
-
-    public String toString() {
-        return basicCube.toString();
+    public void setSide(int side){
+        basicCube.setSide(side);
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public String toString(){
+        String a = "Cube{side=" + getSide() + ", color=\"" + getColor() + "\"}";
+        return a;
+    }
+
+
+    //the following methods have been started for you, but need to be completed
+    public int calculateVolume() {
+        return basicCube.calculateVolume();
+    }
+    public int calculateSurfaceArea() { 
+        return basicCube.calculateSurfaceArea();
+    }
+    public Cube2 add(Cube2 otherCube) {
+        int a = otherCube.getSide();
+        int b = basicCube.getSide();
+        double reality = Math.sqrt(a*a + b*b);
+        int ideal = (int) reality;
+
+        if (reality != ideal){
+            throw new IllegalArgumentException("Sides have to form a pythagorean triple!");
+        }
+        return new Cube2(ideal, this.getColor());
+    }
+
+    public Cube2 minus(Cube2 otherCube) { 
+        int a = otherCube.getSide();
+        int b = basicCube.getSide();
+        double reality = Math.sqrt(b*b-a*a);
+        int ideal = (int) reality;
+
+        if (reality != ideal){
+            throw new IllegalArgumentException("Sides have to form a pythagorean triple!");
+        }
+        return new Cube2(ideal, this.getColor());
+    }
+
+    public boolean equals(Cube2 otherCube) { 
+        if (this.getSide() == otherCube.getSide()&& this.getColor().equals(otherCube.getColor())){
+            return true; 
+        }
+        else{
+            return false;
+        }
+    }
+
+
 }
